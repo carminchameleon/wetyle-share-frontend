3047f1428b288a6c55a0760aa24ce0a312b914f6
aeri <csk647@gmail.com>
2020-02-26 11:05:02 +0900

    제품 상세페이지 제품이름, 사진, 가격, 구매하기

--- src/Routes.js
+++ src/Routes.js
@@ -9,12 +9,14 @@ import {
 // improt 페이지 목록
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Detail from "./Pages/StoreDetail/Detail.js";
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/detail" component={Detail} />
         <Redirect to="/error" />
       </Switch>
     </Router>
