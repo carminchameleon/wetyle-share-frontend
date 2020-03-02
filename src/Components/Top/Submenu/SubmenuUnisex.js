2a5bcdeba5ae6736ef6345d688b283ee7b13c00f
dasoljong92 <56146490+dasoljong92@users.noreply.github.com>
2020-03-02 23:27:37 +0900

    minor html has been added

---
+++ src/Components/Top/Submenu/SubmenuUnisex.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class SubmenuUnisex extends Component {
+  render() {
+    return (
+      <ul className="nav_submenu">
+        <li className="nav_submenu_item">
+          <a>티셔츠</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>후드/집업/맨투맨</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>셔츠</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>니트/가디건</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>아우터</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>팬츠</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>스포츠</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>언더웨어</a>
+        </li>
+        <li className="nav_submenu_item">
+          <a>프리미엄 브랜드</a>
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default SubmenuUnisex;
